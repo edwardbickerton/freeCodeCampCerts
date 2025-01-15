fea18433d9c6fb8d173755eccd7585bd3b58908b
Edward <edwardbickerton@icloud.com>
2025-01-15 17:18:27 +0000

    Solution for Medical Data Visualizer

--- Data Analysis with Python/boilerplate-medical-data-visualizer-main/medical_data_visualizer.py
+++ Data Analysis with Python/boilerplate-medical-data-visualizer-main/medical_data_visualizer.py
@@ -4,57 +4,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df["overweight"] = ((df["weight"] / ((df["height"] / 100) ** 2)) > 25).astype(int)
 
 # 3
+for column in ["cholesterol", "gluc"]:
+    df.loc[df[column] == 1, column] = 0
+    df.loc[df[column] > 1, column] = 1
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = df.melt(
+        id_vars=["id", "cardio"],
+        value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"],
+    )
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]).count()
 
     # 7
-
-
+    df_cat.reset_index(inplace=True)
+    df_cat.rename(columns={"id": "total"}, inplace=True)
 
     # 8
-    fig = None
-
+    fig = sns.catplot(
+        df_cat, x="variable", y="total", hue="value", col="cardio", kind="bar"
+    )
 
     # 9
-    fig.savefig('catplot.png')
+    fig.savefig("catplot.png")
     return fig
 
 
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df.loc[df["ap_lo"] <= df["ap_hi"]]
+    for column in ["height", "weight"]:
+        df_heat = df_heat.loc[
+            (df_heat[column] >= df_heat[column].quantile(0.025))
+            & (df_heat[column] < df_heat[column].quantile(0.975))
+        ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f")
 
     # 16
-    fig.savefig('heatmap.png')
+    fig.savefig("heatmap.png")
     return fig
