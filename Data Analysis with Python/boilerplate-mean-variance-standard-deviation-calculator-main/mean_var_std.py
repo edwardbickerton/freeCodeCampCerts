60dc4d90886af5d6577071bfc5b53ec220a6923c
Edward <edwardbickerton@icloud.com>
2025-01-15 11:11:56 +0000

    Solution for Mean-Variance-Standard Deviation Calculator

--- Data Analysis with Python/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
+++ Data Analysis with Python/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
@@ -1,8 +1,27 @@
 import numpy as np
 
-def calculate(list):
 
+def calculate(lst):
 
+    if len(lst) < 9:
+        raise ValueError("List must contain nine numbers.")
 
+    array = np.array(lst).reshape(3, 3)
+
+    functions = {
+        "mean": np.mean,
+        "variance": np.var,
+        "standard deviation": np.std,
+        "max": np.max,
+        "min": np.min,
+        "sum": np.sum,
+    }
+    calculations = {}
+    for func_name, func in functions.items():
+        calculations[func_name] = [
+            func(array, axis=0).tolist(),
+            func(array, axis=1).tolist(),
+            func(array),
+        ]
 
     return calculations
